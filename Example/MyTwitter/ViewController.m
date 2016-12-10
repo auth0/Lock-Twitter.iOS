cfc3daa58e42a615bc8a27bcd78e846f969fe28a
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-12-10 20:58:30 -0300

    Add Testing app

---
+++ Example/MyTwitter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyTwitter
+//
+//  Created by Hernan Zalazar on 12/7/16.
+//  Copyright © 2016 Hernan Zalazar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Example/MyTwitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyTwitter
+//
+//  Created by Hernan Zalazar on 12/7/16.
+//  Copyright © 2016 Hernan Zalazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
